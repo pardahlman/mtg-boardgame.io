9a074a92b02e714bc3eff1d349781379389ed329
Johannes Bengtsson <jbengtsson@paypal.com>
2020-03-11 08:28:38 +0100

    Add React Testing Library for deep dom testing

--- src/app.test.js
+++ src/app.test.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 
 const disableLogging = testCase => () => {
   jest.spyOn(console, "log").mockImplementation(() => {});
@@ -14,5 +16,14 @@ it(
   disableLogging(() => {
     const div = document.createElement("div");
     ReactDOM.render(<App />, div);
+  })
+);
+
+it(
+  "renders 2 players",
+  disableLogging(() => {
+    const { getByText } = render(<App />);
+    expect(getByText("playerID - 0")).toBeInTheDocument();
+    expect(getByText("playerID - 1")).toBeInTheDocument();
   })
 );
