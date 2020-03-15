8824ed08b5b78526fab99a2ddae1cd55032f4af1
Johannes Bengtsson <bengtsson.johannes@gmail.com>
2020-03-15 18:51:53 +0100

    Add all magic steps

--- src/steps.js
+++ src/steps.js
@@ -1,18 +1,27 @@
 export const STEP = {
-  UNTAP: "untap",
-  UPKEEP: "upkeep"
+  UNTAP: "UNTAP",
+  UPKEEP: "UPKEEP",
+  DRAW: "DRAW",
+  PRECOMBAT_MAIN: "PRECOMBAT_MAIN",
+  BEGINNING_OF_COMBAT: "BEGINNING_OF_COMBAT",
+  DECLARE_ATTACKERS: "DECLARE_ATTACKERS",
+  COMBAT_DAMAGE: "COMBAT_DAMAGE",
+  END_OF_COMBAT: "END_OF_COMBAT",
+  POSTCOMBAT_MAIN: "POSTCOMBAT_MAIN",
+  END: "END",
+  CLEANUP: "CLEANUP"
 };
 
-const steps = [STEP.UNTAP, STEP.UPKEEP];
+const allSteps = Object.values(STEP);
 
-export const getInitialStepState = () => ({ currentStep: steps[0] });
+export const getInitialStepState = () => ({ currentStep: allSteps[0] });
 
 export const setNextMagicStep = (G, ctx) => {
-  const nextStepIndex = steps.indexOf(G.currentStep) + 1;
-  if (nextStepIndex < steps.length) {
-    G.currentStep = steps[nextStepIndex];
+  const nextStepIndex = allSteps.indexOf(G.currentStep) + 1;
+  if (nextStepIndex < allSteps.length) {
+    G.currentStep = allSteps[nextStepIndex];
   } else {
-    G.currentStep = steps[0];
+    G.currentStep = allSteps[0];
     ctx.events.endTurn();
   }
 };
